963043c11716aee2eef7010a724763066ca44c00
Mathew Padley <git@hewley.co.uk>
2026-03-06 11:12:51 +0000

    Refactor DependencyInjection to return IServiceCollection and update namespace

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,13 +1,12 @@
-// Dependency Injection for Application Services
-
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Application;
+namespace ClubMonitor.Application;
 
 public static class DependencyInjection
 {
-    public static void AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // Register services here
+        // register use cases
+        return services;
     }
 }
