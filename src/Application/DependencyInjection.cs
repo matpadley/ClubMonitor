258688ec1f16938679c7114acec8bf0433e5df2a
Mat Padley <mrpadley@icloud.com>
2026-03-06 11:11:53 +0000

    Add DependencyInjection for Application Services

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+// Dependency Injection for Application Services
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        // Register services here
+    }
+}
